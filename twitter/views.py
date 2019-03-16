3d254e5230c608caf915f355da1ade3b67195e3a
agata <agata.marchewka@gmail.com>
2019-03-16 15:44:59 +0100

    views.py updated

--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View, generic
 
+from twitter.models import Tweet
 # Create your views here.
+
+class TweetListView(generic.ListView):
+    template_name = 'twitter/index.html'
+    context_object_name = 'tweets'
+
+    def get_queryset(self):
+        return Tweet.objects.all().order_by('-creation_date')
