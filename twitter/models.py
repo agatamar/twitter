d29fc004ecf5b6a7832d402026c09c459e778097
agata <agata.marchewka@gmail.com>
2019-03-16 14:45:12 +0100

    models.py updated

--- twitter/models.py
+++ twitter/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+TWITTER_MAXIMUM_TWEET_LENGTH = 280
+
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=TWITTER_MAXIMUM_TWEET_LENGTH)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'[{self.creation_date}] ' \
+               f'TWEET by {self.author}: {self.content[:20]}'
