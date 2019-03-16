79c39cd6b971558c7789f4f1de931db2debd3008
agata <agata.marchewka@gmail.com>
2019-03-16 15:44:43 +0100

    main urls.py updated

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from django.views import generic
+from twitter import views
 
 app_name = 'twitter'
 urlpatterns = [
-    path('', generic.TemplateView.as_view(template_name='twitter/index.html'),
+    # path('', generic.TemplateView.as_view(template_name='twitter/index.html'),
+    #      name='index'),
+    path('', views.TweetListView.as_view(),
          name='index'),
+
 ]
